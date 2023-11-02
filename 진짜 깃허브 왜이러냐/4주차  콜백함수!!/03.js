03abbdc2c303efc8d3cce448efdcdc52526865d3
안준표 <dkswnsvy2010@naver.com>
2023-11-02 22:11:25 +0900

    Delete 진짜 깃허브 왜이러냐 directory

--- 진짜 깃허브 왜이러냐/4주차  콜백함수!!/03.js
+++
@@ -1,8 +0,0 @@
-//인자에 대한 제어권을 갖는다
-// map 함수
-var newArr = [10, 20,30].map(function(currentValue, index){
-    console.log(currentValue, index);
-    return currentValue + 5;
-});
-// 결과
-console.log(newArr);
--- 진짜 깃허브 왜이러냐/4주차  콜백함수!!/04.js
+++
@@ -1,5 +0,0 @@
-var newArr = [10, 20, 30].map(function(index, currentValue) { // 매개변수 순서 변경 후 결과 달라짐
-    console.log(index, currentValue);
-    return currentValue + 5;  //컴푸터는 여기를 index로 이해함
-})
-console.log(newArr);
